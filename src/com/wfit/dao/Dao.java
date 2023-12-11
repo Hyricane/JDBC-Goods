7dcf4aaf1551187a2dcaa7a4ab2082609421b673
Hyricane <2825226055@qq.com>
2023-12-11 14:58:51 +0800

    cc

---
+++ src/com/wfit/dao/Dao.java
@@ -0,0 +1,17 @@
+package com.wfit.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dao {
+    public Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://127.0.0.1:3306/student?useUnicode=true&characterEncoding=utf-8";
+        String username = "root";
+        String password = "root";
+        Connection conn = DriverManager.getConnection(url,username,password);
+        return conn;
+    }
+
+}
